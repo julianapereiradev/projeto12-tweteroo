bf2dabea661a9bacd976a811baa4ee987b53ce73
juliana <julianapereira.dev@gmail.com>
2023-06-22 09:03:54 -0300

    POST ang GET for /tweets route almost done. Missing the avatar part and POST conditional

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ app.use(cors());
 app.use(express.json())
 
 const users = []
+const tweets = []
 
 app.post('/sign-up', (req, res) => {
     const user = req.body;
@@ -13,22 +14,18 @@ app.post('/sign-up', (req, res) => {
     res.send("OK")
 })
 
-const tweets = [
-    {
-		username: "bobesponja",
-		avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-		tweet: "Eu amo hambúrguer de siri!"
-	},
-    {
-		username: "patrick",
-		avatar: "https://img.7segundos.com.br/tlQhgF5lXgW4h4uku5PF4Ql4aX4=/1110x650/s3.7segundos.com.br/uploads/imagens/patrick.jpg",
-		tweet: "Eu amo dormir!"
-	}
-];
+console.log("users aqui:", users)
 
 app.get("/tweets", (req, res) => {
   res.send(tweets);
 });
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body
+    tweets.push(tweet)
+    res.send("OK")
+
+})
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}`));
