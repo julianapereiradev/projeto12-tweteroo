db823228b3c14616cf62196709c915f44ad8cbc4
juliana <julianapereira.dev@gmail.com>
2023-06-22 09:34:49 -0300

    FIx: Message UNAUTHORIZED in POST /tweets

--- src/app.js
+++ src/app.js
@@ -3,32 +3,38 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-const users = []
-const tweets = []
-let avatare = ""
+const users = [];
+const tweets = [];
+let _avatar = "";
+let _username = "";
 
-app.post('/sign-up', (req, res) => {
-    const user = req.body;
-    avatare = user.avatar
-    users.push(user)
-    res.send("OK")
-})
+app.post("/sign-up", (req, res) => {
+  const user = req.body;
+  _avatar = user.avatar;
+  users.push(user);
+  res.send("OK");
+});
 
-console.log("users aqui:", users)
+console.log("users aqui:", users);
 
 app.get("/tweets", (req, res) => {
   res.send(tweets);
 });
 
 app.post("/tweets", (req, res) => {
-    const tweet = req.body
-    let neww = {...tweet, avatar: avatare}
-    tweets.push(neww)
-    res.send("OK")
+  const tweet = req.body;
+  _username = tweet.username;
 
-})
+  if (_username === undefined || _username === "") {
+    console.log("UNAUTHORIZED");
+  } else {
+    let tweetWithAvatar = { ...tweet, avatar: _avatar };
+    tweets.push(tweetWithAvatar);
+    res.send("OK");
+  }
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}`));
