e04d46b6beda0d3a1be59937aa4ddc9f451186fb
juliana <julianapereira.dev@gmail.com>
2023-06-22 08:36:10 -0300

    POST requirements for route /sign-up is working

--- src/app.js
+++ src/app.js
@@ -3,6 +3,15 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json())
+
+const users = []
+
+app.post('/sign-up', (req, res) => {
+    const user = req.body;
+    users.push(user)
+    res.send("OK")
+})
 
 const tweets = [
     {
